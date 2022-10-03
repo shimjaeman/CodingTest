d984e4c4f3e27071c7f7b8a90f16405fe3af758c
SJM <95950967+shimjaeman@users.noreply.github.com>
2022-10-03 17:11:06 +0900

    [unrated] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.py
@@ -1,7 +1,2 @@
-def solution(s1, s2):
-    cnt = 0
-    for i in s1:
-        for j in s2:
-            if i == j :
-                cnt +=1
-    return cnt
+def solution (s1, s2):
+    return len(set(s1)&set(s2))
