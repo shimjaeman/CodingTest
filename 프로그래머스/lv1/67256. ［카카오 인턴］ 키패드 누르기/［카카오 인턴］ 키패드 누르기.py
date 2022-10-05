17db242ac42036ccd87189cbfde3ec935ac56d0d
SJM <95950967+shimjaeman@users.noreply.github.com>
2022-10-05 21:33:38 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.81 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -4,41 +4,43 @@ dict_geo = {"1": (0,0), "2": (0,1), "3":(0,2),
             "7": (2,0), "8": (2,1), "9":(2,2),
             "*": (3,0), "0": (3,1), "#":(3,2)}
 
-# 거리 함수계산
-def space(number, pos): 
+def space (number, pos):
     number = str(number)
-    pos = str(pos)  
-    x_number, y_number = dict_geo[number]
-    x_pos, y_pos = dict_geo[pos]
-    return abs(x_number - x_pos) + abs(y_number - y_pos)
+    pos = str(pos)
+    number_x, number_y = dict_geo[number]
+    pos_x, pos_y = dict_geo[pos]
+    return abs(number_x - pos_x) + abs(number_y - pos_y) 
 
-def solution(numbers, hand):
-    R = "#"
-    L = "*"
-    result =""
-    for num in numbers:
+def solution (numbers, hand):
+    left_pos = "*"
+    right_pos = "#"
+    answer = ""
+    for num in numbers :
+        # [1,4,7]
         if num in dict_a["left"]:
-            result += "L"
-            L = str(num)
+            answer += "L"
+            left_pos = str(num)
+        # [3, 6, 9]
         elif num in dict_a["right"]:
-            result += "R"
-            R = str(num)
-        elif num in dict_a["center"] :
-            L_C_space = space(num, L)
-            R_C_space = space(num, R)
-            if L_C_space== R_C_space :
+            answer += "R"
+            right_pos = str(num)
+        # [2, 5, 8, 0]
+        elif num in dict_a["center"]:
+            left_space = space(num, left_pos)
+            right_space = space(num, right_pos)
+            if left_space == right_space:
                 if hand == "right":
-                    result += "R"
-                    R = str(num)
-                else:
-                    result += "L"
-                    L = str(num)
-            elif L_C_space < R_C_space :
-                result += "L"
-                L = str(num)
-            elif L_C_space > R_C_space:
-                result += "R"
-                R = str(num)
-            else : 
+                    answer += "R"
+                    right_pos = str(num)
+                elif hand == "left" :
+                    answer += "L"
+                    left_pos = str(num)
+            elif left_space > right_space:
+                answer += "R"
+                right_pos = str(num) 
+            elif left_space < right_space:
+                answer += "L"
+                left_pos = str(num)
+            else :
                 pass
-    return result
+    return answer
