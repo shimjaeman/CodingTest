e54db98cdb3a0499f9bbbe48b577dec5f9789085
SJM <95950967+shimjaeman@users.noreply.github.com>
2022-10-09 10:53:55 +0900

    [unrated] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,9 +1,10 @@
 def solution(common):
     # 등차수열 확인
-    if common[1] - common[0] == common[2]- common[1]:
+    if common[2]- common[1] == common[1]- common[0]:
         n = common[2]- common[1]
         return common[-1] + (n)
         
     # 등비수열 확인
-    if common[0] * (common[1]//common[0]) == common[1]:
-        return common[-1] * (common[1]//common[0])
+    if common[2]/common[1]==common[1]/common[0]:
+        n = common[2]//common[1]
+        return common[-1] * (n)
