d26a491061f0baede106c44e565d015563998bcf
SJM <95950967+shimjaeman@users.noreply.github.com>
2022-10-13 12:39:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -3,7 +3,7 @@ def solution(s):
     result.append(s[0].upper())
     for i in range(1, len(s)):
         # 숫자가 있고 and 숫자 앞에 
-        if s[i-1] == " ":
+        if s[i-1] == " " and s[i] != " ":
             result.append(s[i].upper())
         else:
             result.append(s[i].lower())
