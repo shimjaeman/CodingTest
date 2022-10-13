00909520a68e8416b106eceff90163e035737af6
SJM <95950967+shimjaeman@users.noreply.github.com>
2022-10-13 12:37:56 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    result = []
+    result.append(s[0].upper())
+    for i in range(1, len(s)):
+        # 숫자가 있고 and 숫자 앞에 
+        if s[i-1] == " ":
+            result.append(s[i].upper())
+        else:
+            result.append(s[i].lower())
+    return "".join(result)
