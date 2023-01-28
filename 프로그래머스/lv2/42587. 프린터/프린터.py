9f7698524ee2323827233c050a6f0df204406195
SJM <95950967+shimjaeman@users.noreply.github.com>
2023-01-28 13:43:08 +0900

    [level 2] Title: 프린터, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,23 +1,13 @@
 from collections import deque
 def solution(priorities, location):
-    my_doc = [0 for _ in range(len(priorities))]
-    my_doc[location] = 1
-    queue = deque(priorities)
-    my_doc = deque(my_doc)
+    atmos = deque([i,j] for i, j in enumerate(priorities))
     answer = 0
-    while True :
-        # 인쇄를 다했거나 해당 문서를 인쇄한 경우
-        if not my_doc or max(my_doc) != 1 :
-            break
-        # 첫번째 문서가 가장 중요도가 높은 경우:
-        if max(queue) == queue[0]:
-            queue.popleft()
-            my_doc.popleft()
-            answer +=1
-        # 첫번째 문서가 가장 중요도가 높은 문서가 아닌경우 
+    while True:
+        front = atmos.popleft()
+        if any (front[1] < doc[1]  for doc in atmos):
+            atmos.append(front)
         else :
-            queue.append(queue[0])
-            queue.popleft()
-            my_doc.append(my_doc[0])
-            my_doc.popleft()   
+            answer +=1
+            if front[0] == location:
+                break
     return answer
