3fabded1b23f0ff9146a2e3f5465660adcee9522
SJM <95950967+shimjaeman@users.noreply.github.com>
2023-01-30 19:46:01 +0900

    [level 2] Title: 2 x n 타일링, Time: 24.68 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,15 +1,5 @@
 def solution(n):
-    class_1 = 1
-    class_2 = 2
-    result = 0
-    if n == 1:
-        return class_1
-    elif n == 2:
-        return class_2
-    else :
-        for i in range(n-2):
-            tmp = class_1
-            class_1 = class_2
-            class_2 = tmp + class_1
-        result = class_2 % 1000000007
-    return result
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = b, a+b
+    return b % 1000000007
